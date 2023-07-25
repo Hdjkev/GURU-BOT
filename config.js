39c058266a850d8c2366390eeee03a5351c602b7
Elisee <122028156+Hdjkev@users.noreply.github.com>
2023-07-25 16:02:29 +0000

    Update config.js
    Me

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['22896479140','seiji nomi🥷', true],
+  ['22896479140', 'seiji', true], 
   [''] 
 ] //Number of owners
 
@@ -47,7 +47,7 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname ='Emperial-bot🥷'
 global.premium = 'true'
 global.packname = 'GURU┃ᴮᴼᵀ' 
 global.author = '@Asliguru' 
